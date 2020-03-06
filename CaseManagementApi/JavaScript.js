303e33b441f8dc0174f6eca98f960e6d7a729b0e
David Karlsson <david.karlsson1@learnet.se>
2020-03-06 11:19:53 +0100

    fetch options  - getCustomer

--- CaseManagementApi/JavaScript.js
+++ CaseManagementApi/JavaScript.js
@@ -34,3 +34,48 @@
 
         })
 }
+
+function getCustomer() {
+fetch('http://localhost:51219/api/customers')
+
+        .then(res => res.json())
+        .then(data => {
+
+            let options = data.map(option => `<option value="${option.id}">${option.status}</option>`)
+        })
+}
+
+
+function newCaseWorker(e) {
+
+    e.preventDefault()
+
+
+
+    var caseworker = {
+        firstname: e.target["firstname"].value,
+        lastname: e.target["lastname"].value,
+        email: e.target["email"].value,
+        phone: e.target["phone"].value,
+
+    }
+
+
+
+    fetch('http://localhost:51219/api/caseworkers', {
+
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.Stringify(caseworker)
+    })
+
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+
+
+        })
+}
